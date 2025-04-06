a50cfce47f0e53013e2792ca5dfb0e65ae4025f5
Богдан Завьялов <buguskin2015@gmail.com>
2025-04-06 15:57:38 +0500

    движение+анимация

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,15 +5,21 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
 
     private Rigidbody2D rb;
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        // Движение
         float moveX = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+
+        // Управление анимацией
+        animator.SetFloat("Speed", Mathf.Abs(moveX));
     }
 }
